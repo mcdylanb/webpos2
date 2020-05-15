1feebbf054cfa547cf5d39d615189e04cbf44c15
mcdylanb <dylansatgalab@gmail.com>
2020-05-15 21:05:08 +0800

    +new branch added JSS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SimpleTable from './TableContainer'
-import InputField from './InputField'
+import {createUseStyles} from 'react-jss'
 
-const App = () => (
-  <div>
-    <InputField/>
-    <SimpleTable/>
+const useStyles = createUseStyles({
+  example: {
+    'background-color': 'red'
+  },
+  grid: {
+    'grid-template-columns': '100px 50px 100px',
+    'grid-template-rows': '80px auto 80px',
+    'column-gap': '10px',
+    'row-gap' : '15px'
+  },
+})
+
+const Product = (props) => {
+  const classes = useStyles()
+  return (
+    <div >
+      <div className={classes.example}> IMG GOES HERE </div>
+      <h1>product name</h1>
+      <h3>Rating</h3>
+      <h3>Tages</h3>
+      <h2>PRICE</h2>
+    </div>
+
+  )
+}
+const App = () => {
+  const classes = useStyles()
+  return (
+   <div className={classes.grid}>
+     <Product/>
+
   </div>
-)
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
