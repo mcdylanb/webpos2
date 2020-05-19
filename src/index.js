94e8e0a6ed8c07e7fb54f4df2e65eca6d69116b0
mcdylanb <dylansatgalab@gmail.com>
2020-05-19 10:30:22 +0800

    removed style, will focus more on displaying data

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createUseStyles} from 'react-jss'
-
-const useStyles = createUseStyles({
-  example: {
-    'background-color': 'red'
-  },
-  grid: {
-    'grid-template-columns': '100px 50px 100px',
-    'grid-template-rows': '80px auto 80px',
-    'column-gap': '10px',
-    'row-gap' : '15px'
-  },
-})
 
 const Product = (props) => {
-  const classes = useStyles()
   return (
     <div >
-      <div className={classes.example}> IMG GOES HERE </div>
+      <div > IMG GOES HERE </div>
       <h1>product name</h1>
       <h3>Rating</h3>
       <h3>Tages</h3>
@@ -28,9 +14,8 @@ const Product = (props) => {
   )
 }
 const App = () => {
-  const classes = useStyles()
   return (
-   <div className={classes.grid}>
+   <div >
      <Product/>
 
   </div>
