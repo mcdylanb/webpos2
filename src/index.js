b9fb0ad4cfceb5ba9441bd76b399aa05f4ec1d75
mcdylanb <dylansatgalab@gmail.com>
2020-05-19 10:41:27 +0800

    added dummy data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const dummy_data = [
+  {
+    id: 1,
+    product: 'Huwawei Matebook 13',
+    rating: 4,
+    price: 959.00
+  },
+  {
+    id: 2,
+    product: 'LG gram ',
+    rating: 4.3,
+    price: 1749.97
+  },
+  {
+    id: 3,
+    product: 'Lenovo Thinkpad P1',
+    rating: 4,
+    price: 1483.74
+  },
+
+]
+
 const Product = (props) => {
   return (
     <div >
